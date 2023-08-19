83c75bdb103cf717f28973693c62ae47bff2a677
RobinHsieh <robin0623.hsieh@gmail.com>
2023-08-19 16:47:50 +0800

    workflow update

--- test/list_filename.py
+++ test/list_filename.py
@@ -20,6 +20,7 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
+    print(os.getcwd())
     if os.path.exists('../token.json'):
         creds = Credentials.from_authorized_user_file('../token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
