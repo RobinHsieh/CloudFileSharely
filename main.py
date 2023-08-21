0e35962affd64b31355c71e0989f61e0bcd2f698
RobinHsieh <robin0623.hsieh@gmail.com>
2023-08-21 21:19:01 +0800

    update

--- main.py
+++ main.py
@@ -14,14 +14,12 @@ for index, spreadsheet_name in enumerate(f_i.spreadsheet_id_dic):
 
 # Access csv files
 pandas.set_option("display.max_rows", 100, "display.max_columns", 20, )
-data_L5 = pandas.read_csv(filepath_or_buffer=f_i.project_path + "/data/L5.csv", sep=",")
 data_L6 = pandas.read_csv(filepath_or_buffer=f_i.project_path + "/data/L6.csv", sep=",")
 data_L9 = pandas.read_csv(filepath_or_buffer=f_i.project_path + "/data/L9.csv", sep=",")
 data_L14 = pandas.read_csv(filepath_or_buffer=f_i.project_path + "/data/L14.csv", sep=",")
 data_L1 = pandas.read_csv(filepath_or_buffer=f_i.project_path + "/data/L1.csv", sep=",")
 
 # Update cells (fill color in sheet boundary)
-up_color.update_cells_color(0, 23, 0.8, 0.8, 0.8, f_i.spreadsheet_id_dic.get("L5.csv"), f_i.singleSheet_namelist[1])
 up_color.update_cells_color(0, 23, 0.8, 0.8, 0.8, f_i.spreadsheet_id_dic.get("L6.csv"), f_i.singleSheet_namelist[2])
 up_color.update_cells_color(0, 23, 0.8, 0.8, 0.8, f_i.spreadsheet_id_dic.get("L9.csv"), f_i.singleSheet_namelist[3])
 up_color.update_cells_color(0, 23, 0.8, 0.8, 0.8, f_i.spreadsheet_id_dic.get("L14.csv"), f_i.singleSheet_namelist[8])
